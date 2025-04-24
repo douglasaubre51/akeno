479e6ea521594b8f608fd954d1c4d2c9dde96ae7
douglasaubre51 <boseallen192@gmail.com>
2025-04-24 19:12:26 +0530

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 
+from . import views
 # from dashboard import views
 
 urlpatterns = [
         # login view
         path('',auth_views.LoginView.as_view()),
 
+        #signup view
+        path('signup',views.get_signup_page),
         # dashboard
         # path('dashboard',views.get_dashboard)
         ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render,redirect
 from django.template import loader
 from django.http import HttpResponse
 
+from .forms import SignupForm
+
 # Create your views here.
 
+def get_signup_page(request):
+
+    if request.method == 'GET':
+
+        form = SignupForm()
+        context = {
+                'form':form
+                }
+        template = loader.get_template('registration/signup.html')
+
+        return HttpResponse(template.render(context,request))
 
