1c6cc5232fcd8108e0a052a018d81a38aa070da8
douglasaubre51 <boseallen192@gmail.com>
2025-04-28 09:59:48 +0530

    created basic layout

--- dashboard/static/js/dashboard/ui.js
+++ dashboard/static/js/dashboard/ui.js
@@ -2,9 +2,14 @@
 
 console.log('starting ui.js!')
 
+// highlight individual tab buttons in content card
 const workers_btn = document.getElementById('workers-btn')
 const projects_btn = document.getElementById('projects-btn')
 
+// initial tab highlight
+workers_btn.classList.add('active-nav-btn')
+
+// toggle highlight
 workers_btn.addEventListener('click',function(){
 
     console.log('clicked workers-btn!')
