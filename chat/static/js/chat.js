b78e7d88214b715aa349c9e19dee075ddb94ae1e
douglasaubre51 <boseallen192@gmail.com>
2025-05-23 09:45:40 +0530

    design user chat room
    successfully

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1,65 +1,63 @@
-"use strict"
-console.log('running chat.js!')
+"use strict";
+console.log("running chat.js!");
 
-let username = document.getElementById('username').value
+let username = document.getElementById("username").value;
 
 // get room guid from hidden field
-let roomGuid = document
-  .getElementById('room-guid')
+let roomGuid = document.getElementById("room-guid");
 
 // create a web socket
 const ws = new WebSocket(
-  'ws://' + window.location.host + '/chat_room/' + roomGuid.value + '/'
-)
+  "ws://" + window.location.host + "/chat_room/" + roomGuid.value + "/",
+);
 
 // connection open
-ws.onopen = function(e) {
-  console.log(`websocket established!`)
-}
+ws.onopen = function (e) {
+  console.log(`websocket established!`);
+};
 
 // closing connection
-ws.onclose = function(e) {
-  console.log(`websocket closed!`) 
-}
+ws.onclose = function (e) {
+  console.log(`websocket closed!`);
+};
 
 // receiving message!
-ws.onmessage = function(e) {
-  const parsedData = JSON.parse(e.data)
-  console.log(`message received! : ${ parsedData.message }`)
+ws.onmessage = function (e) {
+  const parsedData = JSON.parse(e.data);
+  console.log(`message received! : ${parsedData.message}`);
 
   // client side render logic
-  const msg = parsedData.message
-  const senderName = parsedData.username
+  const msg = parsedData.message;
+  const senderName = parsedData.username;
+  const time = parsedData.time;
 
-  const msgBody = document.createElement('span')
-  msgBody.setAttribute('class','msg-body')
-  msgBody.textContent = msg
+  const msgBody = document.createElement("pre");
+  msgBody.setAttribute("class", "msg-body");
+  msgBody.textContent = msg;
 
-  const senderBody = document.createElement('span')
-  senderBody.setAttribute('class','sender-body')
-  senderBody.textContent = senderName
+  const senderBody = document.createElement("span");
+  senderBody.setAttribute("class", "sender-body");
+  senderBody.innerHTML = senderName + " | " + time;
 
-  const msgContainer = document.getElementById('msg-container')
-  msgContainer.append(msgBody)
-  msgContainer.append(senderBody)
-}
+  const msgContainer = document.getElementById("msg-container");
+  msgContainer.append(msgBody);
+  msgContainer.append(senderBody);
+};
 
 // on click send btn
-document
-  .getElementById('send-btn')
-  .addEventListener('click',(e)=>{
-    // send msg via socket!
-    const msgBox = document.getElementById('msg-box')
-    console.log(username)
+document.getElementById("send-btn").addEventListener("click", (e) => {
+  // send msg via socket!
+  const msgBox = document.getElementById("msg-box");
+  console.log(username);
 
-    if(msgBox.value != ''){
-      ws.send(
-	JSON.stringify({
-	  message: msgBox.value,
-	  username: username
-	})
-      )
-    }
+  if (msgBox.value != "") {
+    ws.send(
+      JSON.stringify({
+        message: msgBox.value,
+        username: username,
+      }),
+    );
+  }
 
-  })
-
+  msgBox.value = "";
+});
