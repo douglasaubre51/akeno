58e2b74a7999ffd07590b98712533594705e8fe4
douglasaubre51 <boseallen192@gmail.com>
2025-05-25 09:50:45 +0530

    created worker-dashboard app
    page
    css
    view
    url

--- akeno/settings.py
+++ akeno/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'daphne',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,8 +46,8 @@ INSTALLED_APPS = [
     'authentication',
     'dashboard',
     'user_profile',
-    'chat'
-
+    'chat',
+    'worker_dashboard'
 ]
 
 MIDDLEWARE = [
--- dashboard/static/js/dashboard/ui.js
+++ dashboard/static/js/dashboard/ui.js
@@ -1,7 +1,6 @@
 "use strict";
 console.log("starting ui.js!");
 
-
 // highlight individual tab buttons in content card
 const workers_btn = document.getElementById("workers-btn");
 const projects_btn = document.getElementById("projects-btn");
